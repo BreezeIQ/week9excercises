9a3b755ec451cc62303d3a89bebc5111e261b628
alainwirngo <nsaikila.a@gmail.com>
2018-06-30 16:08:58 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+//var testOne = "What kkind of variable i am"
+   //string
+
+//var testTwo = 256;
+// number 
+
+//var TestThree = false;
+// boolean
+
+//var TestFour = 'true';
+//string
+
+//var testFive = '128';
+//string
+
+//var testSix = "";
+//string
+
+
+
+//excercise #6
+
+
+//var testOne = "hello" + "there";
+// STRING....."hellothere"
+
+var testTwo = '100' + 28;
+//STRING......"10028"
+
+var TestThree = true + 3;
+//NUMBER.....4
+
+var testFour = 'false' +  5;
+//STRING...."false5"
+
+var testFive = '128' -  3;
+//NUMBER ....125
+
+var testSix = "" * 9;
+//NUMBER......0
+
+var testSeven = 'zero' - 1;
+//NaN .....NaN
+
+var testEight = 'five' .length;
+//NUMBER...4
+
+var testNine = 'five'[0] + 'ever';
+//STRING...."fever"
+     
+var testTen = 'a' > 'b'; 
+//false 
